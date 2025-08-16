eb77a10ad6fd0ba6ea934986ea488e46dd82c6ec
wangzhao <zhaowang@xrunda.com>
2025-08-16 23:43:57 +0800

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/20250816/',
+  base: '/web20250816/',
   plugins: [vue()],
   resolve: {
     alias: {
